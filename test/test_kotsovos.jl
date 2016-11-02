796b3fe3bd753816d65f80261279e9f85abf1167
Raul Durand <raul.durand@gmail.com>
2016-11-02 19:22:19 -0400

    test kotsovos updated

--- test/test_kotsovos.jl
+++ test/test_kotsovos.jl
@@ -12,7 +12,6 @@ mesh = Mesh(bl, bl1, bl2, bl3, verbose=verbose)
 
 dom = Domain(mesh)
 
-#set_mat(dom.elems[:solids], ElasticSolid(E=1.e4, nu=0.) )
 set_mat(dom.elems[:solids], Kotsovos(E=20e6, nu=0.25, beta=0.5, fc=20e3, ft=2e3) )
 set_mat(dom.elems[:lines ], Truss(E=2.1e8, A=0.01) )
 set_mat(dom.elems[:joints1D], Joint1D(ks=1.e5, kn=1.e5, A=0.005) )
@@ -23,14 +22,17 @@ set_trackers(dom, node_dat)
 
 bc1 = NodeBC( :(y==0 && z==0), ux=0, uy=0, uz=0)
 bc2 = NodeBC( :(y==6 && z==0), ux=0, uz=0)
-bc3 = FaceBC( :(z==0.4), tz=-20)
-#bc3 = NodeBC( :(y==3.0 && z==0.4), uz=-0.01)
+bc3 = NodeBC( :(y==3.0 && z==0.4), uz=-0.002)
 
 set_bc(dom, bc1, bc2, bc3)
 
-solve!(dom, nincs=10, verbose=verbose, autosave=true)
+solve!(dom, nincs=10, precision=100, verbose=verbose, autosave=true)
+#solve_legacy!(dom, nincs=120, precision=100., verbose=verbose, autosave=true)
 
 #using PyPlot
-#plot(-node_dat.table[:uz], 0:80, marker="o")
 #plot(-node_dat.table[:uz], -node_dat.table[:fz], marker="o")
 #show()
+
+facts("\nTest Kotsovos:") do
+    @fact 1 --> 1
+end
